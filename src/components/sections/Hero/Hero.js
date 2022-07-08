282416f9383677a716657aa752a66af86558bd91
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-08 08:20:10 +0100

    limit viewport

--- src/components/sections/Hero/Hero.js
+++ src/components/sections/Hero/Hero.js
@@ -5,31 +5,33 @@ import styles from "./hero.module.css";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles.contentContainer}>
-        <div className={styles.mainContent}>
-          <h1>WELCOME TO ESPORTCAVE</h1>
+      <div className={styles.heroContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.mainContent}>
+            <h1>WELCOME TO ESPORTCAVE</h1>
 
-          <h2>How About a Game of Whot?</h2>
+            <h2>How About a Game of Whot?</h2>
 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing . Morbi vestibulum mi felis. Cras ac
-            erat arcu.Vivamus sagittis augue ut bibendum mollis.Vivamus sagittis augue ut bibendum
-            mollis.
-          </p>
-          <div className={styles.stores}>
-            <a className="" href="/">
-              <img src="/AppStore-Image.jpeg" alt="app-store" />
-            </a>
-            <a className="" href="/">
-              <img src="/G-play-image.jpeg" alt="google-play-store" />
-            </a>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing . Morbi vestibulum mi felis. Cras
+              ac erat arcu.Vivamus sagittis augue ut bibendum mollis.Vivamus sagittis augue ut
+              bibendum mollis.
+            </p>
+            <div className={styles.stores}>
+              <a className="" href="/">
+                <img src="/AppStore-Image.jpeg" alt="app-store" />
+              </a>
+              <a className="" href="/">
+                <img src="/G-play-image.jpeg" alt="google-play-store" />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <figure className={styles.mainImage}>
-        <img src="cards.png" alt="whot-cards" />
-      </figure>
+        <figure className={styles.mainImage}>
+          <img src="cards.png" alt="whot-cards" />
+        </figure>
+      </div>
     </div>
   );
 };
