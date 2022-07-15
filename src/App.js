236925b3ae003ac0f2888f7c565f1504aab590e9
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-15 16:39:00 +0100

    removing old code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Layout from "./components/Layout";
-// import Hero from "./components/sections/Hero/Hero";
+import Hero from "./components/sections/Hero/Hero";
 // import Earn from "./components/sections/Earn/Earn";
 // import Games from "./components/sections/Games/Games";
 
 //styles
 import "./styles/index.css";
-// import styles from "./styles/main.module.css";
+import styles from "./styles/main.module.css";
 // import Contact from "./components/sections/Contact/Contact";
 import ErrorPage from "./components/sections/ErrorPage/ErrorPage";
 
