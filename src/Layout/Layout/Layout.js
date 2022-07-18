479010025ecbeeb36c58aff68c8845953e9adeff
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-18 05:46:45 +0100

    added meta tags and links favicon

--- src/Layout/Layout/Layout.js
+++ src/Layout/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
@@ -10,14 +10,46 @@ import Head from "next/head";
 const Layout = ({ children }) => {
   const router = useRouter();
 
-  router.asPath;
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    const path = router.asPath;
+
+    const [first, ...title] = path.split("/");
+
+    setPageTitle(title.join("-"));
+  }, [router.asPath]);
 
   return (
     <>
       <Head>
-        <title>EsportsCave | </title>
-        <meta name="description" content="Esportscave Gaming App" />
+        <title>Esportcave | {pageTitle}</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+
+        <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="description" content="Esportcave Gaming App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Esportcave Gaming" />
+        <meta property="og:url" content="https://esportcave.com/" />
+        <meta property="og:description" content="Esportcave Gaming App" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:title" content="Esportcave Gaming" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://esportcave.com/" />
+        <meta property="twitter:url" content="https://esportcave.com/" />
+        <meta name="twitter:description" content="Esportcave Gaming App" />
       </Head>
 
       <Navbar />
