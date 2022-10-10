ff77be5db579c4268c5dc62137681a2a020e41cb
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-10-10 17:38:42 +0100

    added how to play link

--- src/components/atoms/GameCard/GameCard.js
+++ src/components/atoms/GameCard/GameCard.js
@@ -8,15 +8,20 @@ const GameCard = ({ src, title, href, live }) => {
   return (
     <div
       className={`${styles.gameCard} ${live ? styles.live : styles.notLive}`}
-      style={customStyle}
-    >
-      <div className={`${live ? styles.overlay_live : styles.overlay_notLive}`}>
-        <h2>Coming Soon</h2>
-      </div>
+      style={customStyle}>
+      {!live && (
+        <div
+          className={`${live ? styles.overlay_live : styles.overlay_notLive}`}>
+          <h2>Coming Soon</h2>
+        </div>
+      )}
       <figure className={styles.cardImage}>
         <Image src={src} alt={title} width={485} height={356} />
       </figure>
-      <h1 className={styles.cardTitle}>{title}</h1>
+      <div className={styles.cardTitle}>
+        <h1>{title}</h1>
+        <a>How to play?</a>
+      </div>
       <button className={styles.cardBtn}>Play Game</button>
     </div>
   );
