dc546982c64e55f6b963aa303e02de67aa44eb03
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-10-20 23:43:26 +0100

    edited link

--- src/components/Contact/components/MobileContact.js
+++ src/components/Contact/components/MobileContact.js
@@ -9,19 +9,19 @@ const MobileContact = () => {
       <h3>Download Now</h3>
 
       <div className={styles.mobileContainer}>
-        <a href="https://app.esportcave.com/live/eSportcave.apk">
-          <button className={styles.mobileBtn}>
-            <FontAwesomeIcon
-              icon={faGooglePlay}
-              width={24}
-              height={24}
-              color="black"
-            />
-            <div className={styles.mobilePlay}>
-              <span>GET IT ON</span>
-              <span>Google Play</span>
-            </div>
-          </button>
+        <a
+          href="https://app.esportcave.com/live/eSportcave.apk"
+          className={styles.mobileBtn}>
+          <FontAwesomeIcon
+            icon={faGooglePlay}
+            width={24}
+            height={24}
+            color="black"
+          />
+          <div className={styles.mobilePlay}>
+            <span>GET IT ON</span>
+            <span>Google Play</span>
+          </div>
         </a>
         <Link href="#">
           <button className={styles.mobileBtn}>
